6470badbe51f759ad2266a67db0952a935e73728
Travis <travis14@ksu.edu>
2017-09-13 11:23:32 -0500

    9/13 started canvas version of checkers

--- checkers.js
+++ checkers.js
@@ -19,6 +19,8 @@ var state = {
   captures: {w: 0, b: 0}
 }
 
+var ctx;
+
 /** @function getLegalMoves
   * returns a list of legal moves for the specified
   * piece to make.
@@ -34,12 +36,12 @@ function getLegalMoves(piece, x, y) {
     case 'b': // black can only move down the board diagonally
       checkSlide(moves, x-1, y-1);
       checkSlide(moves, x+1, y-1);
-      checkJump(moves, {captures:[],landings:[]}, piece, x, y);
+      checkJump(moves, {captures:[],landings:[], x:x, y:y}, piece, x, y);
       break;
     case 'w':  // white can only move up the board diagonally
       checkSlide(moves, x-1, y+1);
       checkSlide(moves, x+1, y+1);
-      checkJump(moves, {captures:[],landings:[]}, piece, x, y);
+      checkJump(moves, {captures:[],landings:[], x:x, y:y}, piece, x, y);
       break;
     case 'bk': // kings can move diagonally any direction
     case 'wk': // kings can move diagonally any direction
@@ -47,7 +49,7 @@ function getLegalMoves(piece, x, y) {
       checkSlide(moves, x+1, y+1);
       checkSlide(moves, x-1, y-1);
       checkSlide(moves, x+1, y-1);
-      checkJump(moves, {captures:[],landings:[]}, piece, x, y);
+      checkJump(moves, {captures:[],landings:[], x:x, y:y}, piece, x, y);
       break;
   }
   return moves;
@@ -78,6 +80,8 @@ function copyJumps(jumps) {
   // Use Array.prototype.slice() to create a copy
   // of the landings and captures array.
   var newJumps = {
+    x: jumps.x,
+    y: jumps.y,
     landings: jumps.landings.slice(),
     captures: jumps.captures.slice()
   }
@@ -130,13 +134,17 @@ function checkJump(moves, jumps, piece, x, y) {
   * @param {integer} ly - the 'landing' y position of the peice is jumping onto
   */
 function checkLanding(moves, jumps, piece, cx, cy, lx, ly) {
+  // Check that we're not jumping back to our starting position
+  if(lx == jumps.x && ly == jumps.y) return;
   // Check landing square is on grid
   if(lx < 0 || lx > 9 || ly < 0 || ly > 9) return;
   // Check landing square is unoccupied
   if(state.board[ly][lx]) return;
-  // Check capture square is occuped by opponent
-  if((piece === 'b' || piece === 'bk') && !(state.board[cy][cx] === 'w' || state.board[cy][cx] === 'wk')) return;
-  if((piece === 'w' || piece === 'wk') && !(state.board[cy][cx] === 'b' || state.board[cy][cx] === 'bk')) return;
+  // Check capture square is occupied by opponent
+  if(state.turn === 'b' && !(state.board[cy][cx] === 'w' || state.board[cy][cx] === 'wk')) return;
+  if(state.turn === 'w' && !(state.board[cy][cx] === 'b' || state.board[cy][cx] === 'bk')) return;
+  // Check that we haven't landed on this square previously
+  if(0 < jumps.landings.indexOf(function(landing){return landing.x == lx && landing.y == ly;})) return;
   // legal jump! add it to the moves list
   jumps.captures.push({x: cx, y: cy});
   jumps.landings.push({x: lx, y: ly});
@@ -198,54 +206,48 @@ function nextTurn() {
   else state.turn = 'b';
 }
 
-/**@function handleCheckerClick
-	*Click handler for checker
-	*/
-function handleCheckerClick(event){
-	event.preventDefault();
-	var parentId = event.target.parentElement.id;
-	var x = parseInt(parentId.charAt(7));
-	var y = parseInt(parentId.charAt(9));
-	var moves = getLegalMoves(state.board[y][x], x, y);
-	moves.forEach(function(move),{
-		if (move.type === 'slide'){
-			var id = 'checker-' + move.x + '-' + move.y;
-			var square = document.getElementById(id);
-			square.classList.add('highlight');
-		} else{
-			moves.landings.forEach(function(landing){
-			var id = 'checker-' + landing.x + '-' + landing.y;
-			var square = document.getElementById(id);
-			square.classList.add('highlight');
-			});
-		}
-	});
-	console.log(x,y);
-}
-
-/**@function setup
-	*Sets up te game environment
-	*/
-	function setup(){
-		var board = document.createElement('section');
-		board.id = 'game-board';
-		document.body.appendChild(board);
-		for(var y = 0; y < state.board.length; y++){
-			for(var x = 0; x < state.board[y].length; x++){
-				var square = document.createElement('div');
-				square.id = "square-" + x + "-" + y;
-				square.classList.add('square');
-				if((y+x) % 2 === 1) square.classList.add('black');
-				board.appendChild('square');
+function renderBoard(){
+	ctx.fillStyle = '#222';
+	for(var y = 0; y < 10; y++){
+		for(var x = 0; x < 10; x++){
+			if((x + y) % 2 == 1){
+				ctx.fillStyle = '#888';
+				ctx.fillRect(x*100, y*100, 100, 100);
 				if(state.board[y][x]){
-					var checker = document.createElement('div');
-					checker.classList.add('checker');
-					checker.classList.add('checker-' + state.board[y][x]);
-					checker.onclick = handleCheckerClick;
-					square.appendChild(checker);
+					ctx.beginPath();
+					if(state.board[y][x].charAt(0) === 'w'){
+						ctx.fillStyle = '#fff';
+					}
+					else{
+						ctx.fillStyle = '#000';
+					}
+					ctx.arc(x*100+50, y*100+50, 40, 0, Math.PI*2);
+					ctx.fill();
 				}
 			}
 		}
 	}
+}
+
+function hoverOverChecker(event){
+	var x = Math.floor(event.clientX / 100);
+	var y = Math.floor(event.clientY / 100);
+	if(state.board[y][x] && state.board[y][x].charAt(0) === state.turn){
+		ctx.strokeStyle = 'yellow';
+		ctx.beginPath();
+		ctx.arc(x*100+50, y*100+50, 40, 0, Math.PI*2);
+		ctx.stroke();
+	}
+}
+
+function setup(){
+	var canvas = dpcument.createElelment('canvas');
+	canvas.width = 1000;
+	canvas.height = 1000;
+	canvas.onmousemove = hoverOverChecker;
+	document.body.appendChild(canvas);
+	ctx = canvas.getContext('2d');
+	renderBoard();
+}
 
 setup();
